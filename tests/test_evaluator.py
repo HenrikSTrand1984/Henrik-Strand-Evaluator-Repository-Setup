30990053e1ee84c266f4f6e077a2100aeec9ebc9
Henrik Strand <160159466+HenrikSTrand1984@users.noreply.github.com>
2025-08-05 12:57:26 +0200

    Update test_evaluator.py

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -1,1 +1,15 @@
+import pytest
+from src.evaluator import calc_rammeavtale_score, calc_prosjekt_score, calc_total_score
 
+
+def test_rammeavtale():
+    assert round(calc_rammeavtale_score(91, 90, 82.5, 95, 90, 85), 2) == 67.05
+
+
+def test_prosjekt():
+    assert calc_prosjekt_score(88) == 22.0
+
+
+def test_total():
+    total = calc_total_score(91, 90, 82.5, 95, 90, 85, 88)
+    assert total == 89.05
